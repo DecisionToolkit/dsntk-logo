b8e659fb4db77824fa6d2c60c7df12bd782655c2
dsntk <149608688+dsntk@users.noreply.github.com>
2024-03-05 17:46:39 +0100

    Upgraded dependencies.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use domrs::{HtmlBodyElement, HtmlDocument, HtmlElement, HtmlHeadElement, SvgElement};
+use domrs::{HtmlBodyElement, HtmlDocument, HtmlElement, HtmlHeadElement, SvgDocument, SvgNumber};
 use std::f64::consts::PI;
 use std::fmt::Write;
 
@@ -26,12 +26,12 @@ fn get_path_points(x: f64, y: f64, r: f64) -> Vec<(f64, f64)> {
 
 fn get_svg_rect(x: f64, y: f64, width: f64, height: f64, bg_color: &str) -> HtmlElement {
   let mut rect = HtmlElement::new("rect");
-  rect.set_attr("x", format!("{:.1}", x));
-  rect.set_attr("y", format!("{:.1}", y));
-  rect.set_attr("width", format!("{:.1}", width));
-  rect.set_attr("height", format!("{:.1}", height));
-  rect.set_attr("stroke", "none");
-  rect.set_attr("fill", bg_color);
+  rect.set_attribute("x", format!("{:.1}", x));
+  rect.set_attribute("y", format!("{:.1}", y));
+  rect.set_attribute("width", format!("{:.1}", width));
+  rect.set_attribute("height", format!("{:.1}", height));
+  rect.set_attribute("stroke", "none");
+  rect.set_attribute("fill", bg_color);
   rect
 }
 
@@ -45,26 +45,30 @@ fn get_svg_path(points: &[(f64, f64)], fg_color: &str) -> HtmlElement {
   let _ = write!(&mut d, " L {:.1},{:.1}", points[5].0, points[5].1);
   let _ = write!(&mut d, " Z");
   let mut path = HtmlElement::new("path");
-  path.set_attr("d", d);
-  path.set_attr("stroke", "none");
-  path.set_attr("fill", fg_color);
+  path.set_attribute("d", d);
+  path.set_attribute("stroke", "none");
+  path.set_attribute("fill", fg_color);
   path
 }
 
 fn get_svg_line(x1: f64, y1: f64, x2: f64, y2: f64, bg_color: &str, line_width: f64) -> HtmlElement {
   let mut line = HtmlElement::new("line");
-  line.set_attr("x1", format!("{:.1}", x1));
-  line.set_attr("y1", format!("{:.1}", y1));
-  line.set_attr("x2", format!("{:.1}", x2));
-  line.set_attr("y2", format!("{:.1}", y2));
-  line.set_attr("stroke", bg_color);
-  line.set_attr("stroke-width", format!("{:.1}", line_width));
-  line.set_attr("stroke-linecap", "square");
+  line.set_attribute("x1", format!("{:.1}", x1));
+  line.set_attribute("y1", format!("{:.1}", y1));
+  line.set_attribute("x2", format!("{:.1}", x2));
+  line.set_attribute("y2", format!("{:.1}", y2));
+  line.set_attribute("stroke", bg_color);
+  line.set_attribute("stroke-width", format!("{:.1}", line_width));
+  line.set_attribute("stroke-linecap", "square");
   line
 }
 
 fn get_svg(width: f64, height: f64, line_width: f64) -> HtmlElement {
-  let mut svg: HtmlElement = SvgElement::default().with_width(format!("{:.1}", width)).with_height(format!("{:.1}", height)).into();
+  let mut svg: HtmlElement = SvgDocument::new()
+    .default_namespace()
+    .width(SvgNumber::new(width, 1))
+    .height(SvgNumber::new(height, 1))
+    .into();
 
   let w_2 = width / 2.0;
   let h_2 = height / 2.0;
@@ -112,15 +116,15 @@ fn get_svg(width: f64, height: f64, line_width: f64) -> HtmlElement {
 }
 
 fn main() {
-  let head = HtmlHeadElement::default().with_charset("UTF-8").with_title("DSNTK LOGO");
+  let head = HtmlHeadElement::default().charset("UTF-8").title("DSNTK LOGO");
 
   let mut body = HtmlBodyElement::default();
   body.add_child(get_svg(700.0, 700.0, 9.0));
-  body.add_br();
+  body.add_child(HtmlElement::br());
   body.add_child(get_svg(400.0, 400.0, 7.0));
-  body.add_br();
+  body.add_child(HtmlElement::br());
   body.add_child(get_svg(100.0, 100.0, 3.0));
 
-  let doc = HtmlDocument::new("en", head.into(), body.into());
-  doc.save("./out/dsntk-logo.html").expect("writing file failed");
+  let doc = HtmlDocument::new().default_doctype().default_language().default_namespace().head(head).body(body);
+  doc.save("./out/dsntk-logo.html", 0, 2).expect("writing file failed");
 }
